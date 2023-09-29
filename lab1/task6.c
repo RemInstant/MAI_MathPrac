e9d6ad4d41e997115a08ce7193076879f6dc1d94
RemInstant <pboykov04@mail.ru>
2023-09-29 23:38:05 +0300

    lab1 task6 refactor

--- lab1/task6.c
+++ lab1/task6.c
@@ -3,110 +3,198 @@
 #include <math.h>
 #include <ctype.h>
 
-int validate_string_float(char* str_float) {
+typedef enum
+{
+	OK,
+	INVALID_INPUT,
+	OVERFLOW,
+	BAD_ALLOC
+} status_codes;
+
+status_codes validate_string_double(char* str_double)
+{
 	int dot_cnt = 0;
-	for (int i = 0; str_float[i] != '\0'; ++i) {
-		if (i == 0 && str_float[i] == '-') continue;
-		if (dot_cnt > 1) return 0;
-		if (str_float[i] == '.') {
-			++dot_cnt;
-			continue;
-		}
-		if (isdigit(str_float[i])) continue;
-		return 0;
+	
+	if (!isdigit(str_double[0]) && str_double[0] != '-')
+	{
+		return INVALID_INPUT;
 	}
-	return 1;
+	
+	for (int i = 1; str_double[i]; ++i)
+	{
+		if (str_double[i] == '.')
+		{
+			++dot_cnt;
+			if (dot_cnt > 1) 
+			{
+				return INVALID_INPUT;
+			}
+		}
+		else if (!isdigit(str_double[i]))
+		{
+			return INVALID_INPUT;
+		}
+	}
+	return OK;
 }
 
-double integrate_a(double eps) {
+status_codes integrate_a(double eps, double* ans)
+{
+	if (ans == NULL)
+	{
+		return INVALID_INPUT;
+	}
+	
 	// right Riemann sum
+	unsigned long long step_cnt = 1;
 	double part_len = 1;
-	double step = 1;
 	double prev, cur = log(2);
 	
-	do {
+	do
+	{
+		if (step_cnt > ULLONG_MAX / 2)
+		{
+			return OVERFLOW;
+		}
+		
 		prev = cur;
+		step_cnt *= 2;
 		part_len /= 2;
-		step /= 2;
 		
 		cur = 0;
-		for (double x = 1; x > 0; x -= step)
-			cur += log(1+x)/x * part_len;
 		
+		for (int i = step_cnt; i > 0; --i)
+		{
+			double x = i * 1.0 / step_cnt;
+			cur += log(1+x) / x * part_len;
+		}
 	} while (fabs(cur - prev) >= eps);
 	
-	return cur;
+	*ans = cur;
+	
+	return OK;
 }
 
-double integrate_b(double eps) {
+status_codes integrate_b(double eps, double* ans)
+{
+	if (ans == NULL)
+	{
+		return INVALID_INPUT;
+	}
+	
 	// right Riemann sum
+	unsigned long long step_cnt = 1;
 	double part_len = 1;
-	double step = 1;
 	double prev, cur = exp(-0.5);
 	
-	do {
+	do
+	{
+		if (step_cnt > ULLONG_MAX / 2)
+		{
+			return OVERFLOW;
+		}
+		
 		prev = cur;
+		step_cnt *= 2;
 		part_len /= 2;
-		step /= 2;
 		
 		cur = 0;
-		for (double x = 1; x > 0; x -= step)
-			cur += exp(-x*x * 0.5) * part_len;
 		
+		for (int i = step_cnt; i > 0; --i)
+		{
+			double x = i * 1.0 / step_cnt;
+			cur += exp(-x*x * 0.5) * part_len;
+		}
 	} while (fabs(cur - prev) >= eps);
 	
-	return cur;
+	*ans = cur;
+	return OK;
 }
 
-double integrate_c(double eps) {
+status_codes integrate_c(double eps, double* ans)
+{
+	if (ans == NULL)
+	{
+		return INVALID_INPUT;
+	}
+	
 	// left Riemann sum
+	unsigned long long step_cnt = 1;
 	double part_len = 1;
-	double step = 1;
 	double prev, cur = 0;
 	
-	do {
+	do
+	{
+		if (step_cnt > ULLONG_MAX / 2)
+		{
+			return OVERFLOW;
+		}
+		
 		prev = cur;
+		step_cnt *= 2;
 		part_len /= 2;
-		step /= 2;
 		
 		cur = 0;
-		for (double x = 0; x < 1; x += step)
-			cur += log(1.0 / (1 - x)) * part_len;
 		
+		for (int i = 0; i < step_cnt; ++i)
+		{
+			double x = i * 1.0 / step_cnt;
+			cur += log(1.0 / (1 - x)) * part_len;
+		}
 	} while (fabs(cur - prev) >= eps);
 	
-	return cur;
+	*ans = cur;
+	return OK;
 }
 
-double integrate_d(double eps) {
+status_codes integrate_d(double eps, double* ans)
+{
+	if (ans == NULL)
+	{
+		return INVALID_INPUT;
+	}
+	
 	// right Riemann sum
+	unsigned long long step_cnt = 1;
 	double part_len = 1;
-	double step = 1;
 	double prev, cur = 1;
 	
-	do {
+	do
+	{
+		if (step_cnt > ULLONG_MAX / 2)
+		{
+			return OVERFLOW;
+		}
+		
 		prev = cur;
+		step_cnt *= 2;
 		part_len /= 2;
-		step /= 2;
 		
 		cur = 0;
-		for (double x = 1; x > 0; x -= step)
-			cur += pow(x, x) * part_len;
 		
+		for (int i = step_cnt; i > 0; --i)
+		{
+			double x = i * 1.0 / step_cnt;
+			cur += pow(x, x) * part_len;
+		}
 	} while (fabs(cur - prev) >= eps);
 	
-	return cur;
+	*ans = cur;
+	return OK;
 }
 
-int main(int argc, char** argv) {
-	if (argc == 1) {
+int main(int argc, char** argv)
+{
+	if (argc == 1)
+	{
 		printf("Usage: command_name <eps>\n");
 		printf("application computes some integrals named a, b, c and d...\n");
 		printf("I hope, you know those four as well\n");
 		return 0;
 	}
 	
-	if (argc != 2) {
+	if (argc != 2)
+	{
 		printf("Invalid input\n");
 		return 1;
 	}
@@ -114,20 +202,55 @@ int main(int argc, char** argv) {
 	char* string_eps = argv[1];
 	double eps;
 	
-	if (!validate_string_float(string_eps)) {
+	if (validate_string_double(string_eps) != OK)
+	{
 		printf("Invalid float\n");
 		return 2;
 	}
 	
 	eps = atof(string_eps);
 	
-	if (eps <= 0) {
+	if (eps <= 0)
+	{
 		printf("Epsilon must be positive\n");
 		return 3;
 	}
 	
-	printf("the value of integral a is %.15lf\n", integrate_a(eps));
-	printf("the value of integral b is %.15lf\n", integrate_b(eps));
-	printf("the value of integral c is %.15lf\n", integrate_c(eps));
-	printf("the value of integral d is %.15lf\n", integrate_d(eps));
+	double ans;
+	
+	if (integrate_a(eps, &ans) == OK)
+	{
+		printf("The value of the integral a is %.15lf\n", ans);
+	}
+	else
+	{
+		printf("Calculating the integral a caused overflow\n");
+	}
+	
+	if (integrate_b(eps, &ans) == OK)
+	{
+		printf("The value of the integral b is %.15lf\n", ans);
+	}
+	else
+	{
+		printf("Calculating the integral b caused overflow\n");
+	}
+	
+	if (integrate_c(eps, &ans) == OK)
+	{
+		printf("The value of the integral c is %.15lf\n", ans);
+	}
+	else
+	{
+		printf("Calculating the integral c caused overflow\n");
+	}
+	
+	if (integrate_d(eps, &ans) == OK)
+	{
+		printf("The value of the integral d is %.15lf\n", ans);
+	}
+	else
+	{
+		printf("Calculating the integral d caused overflow\n");
+	}
 }
