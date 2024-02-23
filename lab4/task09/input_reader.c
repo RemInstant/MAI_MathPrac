0fcc09d83cea7da39edd3ce25232e0a6e165435d
RemInstant <pboykov04@mail.ru>
2024-02-23 21:36:28 +0300

    lab4 task9 micro-push of input reader module

---
+++ lab4/task09/input_reader.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "input_reader.h"
+
+// SOOO RAW CODE, DO NOT WATCH IT
+
+status_code ir_set_null(Input_reader* ir)
+{
+	if (ir == NULL)
+	{
+		return NULL_ARG;
+	}
+	ir->data_queue = NULL;
+	return OK;
+}
+
+status_code ir_construct(Input_reader* ir)
+{
+	if (ir == NULL)
+	{
+		return NULL_ARG;
+	}
+	
+	ir->data_queue = (p_queue*) malloc(sizeof(p_queue));
+	if (ir->data_queue == NULL)
+	{
+		return BAD_ALLOC;
+	}
+	
+	p_queue_set_null(ir->data_queue);
+	p_queue_init(ir->data_queue, PQB_BINOM);
+}
+
+status_code ir_destruct(Input_reader* ir)
+{
+	
+}
+
+
+status_code ir_read_file(Input_reader* ir, const char* path);
+status_code ir_get_up_to(Input_reader* ir, const char time[21], size_t* req_cnt, request** reqs);
---
+++ lab4/task09/input_reader.h
@@ -0,0 +1,19 @@
+#ifndef _INPUT_READER_H_
+#define _INPUT_READER_H_
+
+#include "utility.h"
+#include "priority_queue/priority_queue.h"
+
+typedef struct Input_reader
+{
+	p_queue* data_queue;
+} Input_reader;
+
+status_code ir_set_null(Input_reader* ir);
+status_code ir_construct(Input_reader* ir);
+status_code ir_destruct(Input_reader* ir);
+
+status_code ir_read_file(Input_reader* ir, const char* path);
+status_code ir_get_up_to(Input_reader* ir, const char time[21], size_t* req_cnt, request** reqs);
+
+#endif // _INPUT_READER_H_
